b94116db7c414c0ea04e10e7ef503bff3e5b9c17
Simón Botero <sboteroarango@gmail.com>
2021-06-18 16:54:59 -0500

    Create reporteCovid19.py

---
+++ reporteCovid19.py
@@ -0,0 +1,33 @@
+import requests
+from datetime import datetime
+
+
+diaDeHoy = (datetime.now()).day
+pagina = (requests.get("https://api.covid19api.com/total/dayone/country/colombia")).json()
+penUltimoReporte = pagina[-2]
+ultimoReporte = pagina[-1]
+
+casos = ultimoReporte["Confirmed"]
+fallecidos = ultimoReporte["Deaths"]
+recuperados = ultimoReporte["Recovered"] 
+activos = ultimoReporte["Active"]
+
+nuevosFallecidos = ultimoReporte["Deaths"] -penUltimoReporte["Deaths"]
+nuevosCasos = ultimoReporte["Confirmed"] -penUltimoReporte["Confirmed"]
+nuevosRecuperados = ultimoReporte["Recovered"] -penUltimoReporte["Recovered"]
+
+
+
+print(f"""#ReporteCOVID19 dia {diaDeHoy} del mes:/n{nuevosRecuperados:,} recuperados
+{nuevosCasos:,} nuevos casos
+{nuevosFallecidos:,} fallecidos
+
+
+Total:
+
+{recuperados:,} recuperados
+{casos:,} casos 
+{fallecidos:,} fallecidos 
+18,557,906 muestras procesadas
+{activos:,} casos activos""")
+
